2c897e7be0882075fdeca6729fd8ec13ebb820fe
YuransiS <yura3zaxar@outlook.com>
2026-03-25 17:48:26 +0200

    "update_pixel_id"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1603339214309331');
+              fbq('init', '925687026840653');
               fbq('track', 'PageView');
             `,
           }}
@@ -61,7 +61,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1603339214309331&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=925687026840653&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
